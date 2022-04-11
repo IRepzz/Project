e17085ffc96bcf40c6ed84f24a8db7b8445536f5
Daniel Minchev <danielgalinminchev@gmail.com>
2022-04-11 20:07:45 +0300

    [Day 2]

---
+++ project-1/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent]
+})
+export class AppModule { }
